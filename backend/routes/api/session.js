7eea9a98b243e7716dd248bc5045dc19ac6f0efd
JehanIbrahem <jibrahem@ucsb.edu>
2023-07-11 14:03:09 -0700

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -39,6 +39,14 @@ router.post(
             user: safeUser
         });
     }
-);
+    );
+
+        router.delete(
+            '/',
+            (_req, res) => {
+                res.clearCookie('token');
+                return res.json({ message: 'success' });
+            }
+        );
 
 module.exports = router;
