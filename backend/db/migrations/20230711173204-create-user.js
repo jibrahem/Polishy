6ec8fdcac42f3eda124713cc95f95bdbcb9d505d
JehanIbrahem <jibrahem@ucsb.edu>
2023-07-11 22:42:09 -0700

    fixed user migration

--- backend/db/migrations/20230711173204-create-user.js
+++ backend/db/migrations/20230711173204-create-user.js
@@ -5,6 +5,9 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
+options.tableName = "Users";
+
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
@@ -40,7 +43,6 @@ module.exports = {
     }, options);
   },
   async down(queryInterface, Sequelize) {
-    options.tableName = "Users";
     return queryInterface.dropTable(options);
   }
 };
