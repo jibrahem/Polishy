2bd0cc3fa872b6e71ab76e8964d1941981ae9790
JehanIbrahem <jibrahem@ucsb.edu>
2023-07-17 16:54:59 -0700

    fixed the nav bar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -38,36 +38,39 @@ function Navigation({ isLoaded }) {
             <nav>
                 <div className='nav'>
                     <ul>
-                        <li>
-                            <div className='polishy'>
-                                <NavLink exact to="/">Polishy</NavLink>
-                            </div>
-                        </li>
-                        <li>
-                            <div className='search'>
-                                <input type="search"
-                                    placeholder='   Search for anything'
-                                />
-                                <button type='submit'>
-                                    <i class="fa-solid fa-magnifying-glass">
-                                    </i></button>
-                            </div>
-                        </li>
-                        <div className='cart_profile'>
-                            {isLoaded && (
-                                <li>
-                                    <ProfileButton user={sessionUser} />
-                                </li>
-                            )}
-                            {sessionUser &&
-                                <div className='shopping-cart'>
-                                    <li>
-                                        <button onClick={onClick}>
-                                            <i class="fa-solid fa-cart-shopping"></i>
-                                        </button>
-                                    </li>
+                        <div className='top'>
+                            <li>
+                                <div className='polishy'>
+                                    <NavLink exact to="/">Polishy</NavLink>
                                 </div>
-                            }
+                            </li>
+                            <li>
+                                <div className='search'>
+                                    <input type="search"
+                                        placeholder='   Search for anything'
+                                    />
+                                    <button type='submit'>
+                                        <i class="fa-solid fa-magnifying-glass">
+                                        </i></button>
+                                </div>
+                            </li>
+                            <div className='cart_profile'>
+                                {isLoaded && (
+
+                                    <li>
+                                        <ProfileButton user={sessionUser} />
+                                    </li>
+                                )}
+                                {sessionUser &&
+                                    <div className='shopping-cart'>
+                                        <li>
+                                            <button onClick={onClick}>
+                                                <i class="fa-solid fa-cart-shopping"></i>
+                                            </button>
+                                        </li>
+                                    </div>
+                                }
+                            </div>
                         </div>
                     </ul>
                 </div>
